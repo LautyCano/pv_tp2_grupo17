a728b026e25bd1d7ab0528c30d9968adfb4aba3c
peluk467 <peluk467@gmail.com>
2025-04-13 21:46:13 -0300

    Ejercicio 8,9,10 Lucas Adrian Fernandez

---
+++ ejercicio10.js
@@ -0,0 +1,10 @@
+
+let num1 = parseInt(prompt("Ingrese el primer número entero:"));
+let num2 = parseInt(prompt("Ingrese el segundo número entero:"));
+let num3 = parseInt(prompt("Ingrese el tercer número entero:"));
+
+
+let promedio = (num1 + num2 + num3) / 3;
+
+
+alert("El promedio de los tres números es: " + promedio);
