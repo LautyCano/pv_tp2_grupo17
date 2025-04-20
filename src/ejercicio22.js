4b6568aeecbe2530aad2fd182fbb819c057c35ba
Lucas2007040507 <capitanchorizo1234@gmail.com>
2025-04-20 17:13:46 -0300

    Ejercicio 22 Lucas Luciano Camino

--- src/ejercicio22.js
+++ src/ejercicio22.js
@@ -1,6 +1,8 @@
 import { actualizarTexto } from "pv_tp2_grupo17/src/ejercicio21.js";
+import { cambiarColor } from "pv_tp2_grupo17/src/ejercicio22.js";
 
 const input = document.getElementById("inputTexto");
 const textoMostrado = document.getElementById("textoMostrado");
 
 actualizarTexto(input, textoMostrado);
+cambiarColor(input, textoMostrado);
